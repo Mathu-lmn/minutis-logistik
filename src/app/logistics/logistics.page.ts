87ad3013f899f28641ed70abb29058d95718a933
Valentin <valentin.gourjon@efrei.net>
2024-04-08 17:37:48 +0200

    jpp

--- src/app/logistics/logistics.page.ts
+++ src/app/logistics/logistics.page.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonLabel, IonSegmentButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {LogLMyDemandsComponent} from './log-l/log-l-my-demands/log-l-my-demands.component';
 
 @Component({
   selector: 'app-logistics',
   templateUrl: 'logistics.page.html',
   styleUrls: ['logistics.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonLabel, IonTitle, IonContent, IonSegment, IonSegmentButton, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonLabel, IonTitle, IonContent, IonSegment, IonSegmentButton, ExploreContainerComponent, LogLMyDemandsComponent]
 })
 export class LogisticsPage {
 
