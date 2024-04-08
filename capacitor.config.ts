3877cba861d50e4910dd4e89ae3dcdddffdbb562
Mathu-lmn <80094438+Mathu-lmn@users.noreply.github.com>
2024-04-08 13:53:51 +0200

    Update repo again

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
+  appName: 'minutis-logistik',
   webDir: 'www',
   server: {
     androidScheme: 'https'
