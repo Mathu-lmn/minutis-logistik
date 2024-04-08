307d35be3655e424cb701ba52b20d758ba47ed62
Niilyx <nait.meddour.idir100@gmail.com>
2024-04-08 16:37:02 +0200

    segment made functional

--- src/app/logistics/logistics.page.ts
+++ src/app/logistics/logistics.page.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonLabel, IonSegmentButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-logistics',
   templateUrl: 'logistics.page.html',
   styleUrls: ['logistics.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonLabel, IonTitle, IonContent, IonSegment, IonSegmentButton, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonLabel, IonTitle, IonContent, IonSegment, IonSegmentButton, ExploreContainerComponent, CommonModule]
 })
 export class LogisticsPage {
+  segmentValue: string;
+  constructor() {
+    this.segmentValue = 'all-orders';
+  }
 
-  constructor() {}
+  segmentChanged(event: any) {
+    this.segmentValue = event.detail.value;
+  }
 
 }
