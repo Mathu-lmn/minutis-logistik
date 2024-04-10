dc4b40787084697f2c51b48fbabeb124bce5c080
Niilyx _ <43385037+Niilyx@users.noreply.github.com>
2024-04-10 02:11:31 +0200

    grosse avancée

--- src/app/logistics/log-l/my-demands/my-demands.component.ts
+++ src/app/logistics/log-l/my-demands/my-demands.component.ts
@@ -1,22 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  IonBadge,
-  IonIcon,
-  IonContent,
-  IonText,
-  IonModal,
-  IonList,
-  IonLabel,
-  IonItem,
-  IonInfiniteScroll,
-  IonToolbar,
-  IonHeader,
-  IonTitle,
-  IonButton
-} from '@ionic/angular/standalone';
+import * as I from '@ionic/angular/standalone';
 import { closeOutline } from 'ionicons/icons';
-import { Demand } from '../../types';
+import { Demand, DemandStatus } from '../../types';
 import { addIcons } from 'ionicons';
 
 import { demands } from '../../dummy';
@@ -30,50 +16,83 @@ import { MapComponent } from 'src/app/map/map.component';
   styleUrls: ['./my-demands.component.scss'],
   standalone: true,
   imports: [
-    IonBadge,
-    IonIcon,
-    IonContent,
-    IonText,
-    IonList,
-    IonLabel,
-    IonItem,
-    IonInfiniteScroll,
-    IonToolbar,
-    IonHeader,
-    IonTitle,
-    IonModal,
+    MapComponent,
+    I.IonGrid,
+    I.IonRow,
+    I.IonBadge,
+    I.IonIcon,
+    I.IonContent,
+    I.IonText,
+    I.IonList,
+    I.IonLabel,
+    I.IonItem,
+    I.IonInfiniteScroll,
+    I.IonToolbar,
+    I.IonHeader,
+    I.IonTitle,
+    I.IonModal,
+    I.IonCard,
+    I.IonCardHeader,
+    I.IonCardTitle,
+    I.IonCardContent,
     CommonModule,
-    IonButton,
+    I.IonButton,
   ],
 })
-export class LogLMyDemandsComponent implements OnInit {
-  demands: Demand[];
+export class LogLMyDemandsComponent implements OnInit, AfterViewInit {
+  myDemands: Demand[];
+  isCurrentlyShipping: boolean = false;
   
-  @ViewChild(IonModal) modal: any;
+  @ViewChildren(I.IonModal) modals: any[] = [];
 
   constructor(public utils: Utils) {
-    this.demands = demands;
+    this.myDemands = demands;
     addIcons({
-      closeOutline
+      closeOutline,
     });
   }
-
+  
   getProducts(demand: Demand) {
     return demand.content
-      .map((item: any) => item.label + ' (' + item.quantity + ')')
-      .join(', ');
+    .map((item: any) => item.label + ' (' + item.quantity + ')')
+    .join(', ');
   }
-
+  
   showDemand(demand: Demand) {
     console.log('showDemand', demand);
   }
-
+  
   log(demand: Demand) {
     console.log(demand);
   }
   
+  markAsShipping(event: Event) {
+    if (this.checkIfCurrentlyShipping()) {
+      // erreur OU return; pour annuler l'action
+    }
+    for (let myDemand of this.myDemands) {
+      // envoi d'un message au backend, puis...
+      myDemand.status = DemandStatus.Shipping;
+    }
+    this.ngOnInit()
+  }
 
+  checkIfCurrentlyShipping() {
+    // ici call au backend
+    this.isCurrentlyShipping = !this.isCurrentlyShipping;
+    return this.isCurrentlyShipping;
+  }
+
+  dismissModal() {
+    this.modals.forEach(m => m.dismiss(null, 'cancel'))
+  }
+  
   // lint:disable-next-line: use-lifecycle-interface
   ngOnInit() {}
+  
+  ngAfterViewInit(): void {
+    // this.checkIfCurrentlyShipping();
+    console.log("isShipping=" + this.isCurrentlyShipping);
+  }
 
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,24 +9,40 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements AfterViewInit {
   private map: any;
-  @Input() lat?: number; // = 45.7431141144872
-  @Input() lng?: number; // = 4.22320976350099;
+  @Input({ transform: parseFloat }) lat?: number; // = 45.7431141144872
+  @Input({ transform: parseFloat }) lng?: number; // = 4.22320976350099;
+  @Input({ transform: parseInt }) mapId?: number;
 
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ this.lat ? this.lat : 1, this.lng ? this.lng : 1 ],
-      zoom: 3
+    this.map = L.map(`map${this.mapId}`).on("load", () => {
+      setTimeout(() => {
+        this.map.invalidateSize();
+      }, 50);
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
+      maxZoom: 25,
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
     tiles.addTo(this.map);
+    
+    this.map.setView([ this.lat ? this.lat : 1, this.lng ? this.lng : 1 ], 16);
+    
+    L.Icon.prototype.options = {
+      ...L.Icon.prototype.options,
+      shadowUrl: "assets/marker-shadow.png",
+      // iconUrl: "",
+      iconUrl: "assets/marker-icon.png",
+      iconSize: [26, 41],
+      iconAnchor: [13, 41],
+    }
+    L.marker([ this.lat ? this.lat : 1, this.lng ? this.lng : 1 ], {
+      icon: L.icon(L.Icon.prototype.options)
+    }).addTo(this.map);
   }
-
+  
   constructor() { }
 
   ngAfterViewInit(): void {
